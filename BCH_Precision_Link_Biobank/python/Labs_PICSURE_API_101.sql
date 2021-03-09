b173f434fc7867c0a032932032afdc38b0e046aa
Jaspreet Khanna <Jaspreet.Khanna@childrens.harvard.edu>
2021-03-09 16:38:42 -0500

    Update Labs_PICSURE_API_101.sql

--- BCH_Precision_Link_Biobank/python/Labs_PICSURE_API_101.sql
+++ BCH_Precision_Link_Biobank/python/Labs_PICSURE_API_101.sql
@@ -2,41 +2,39 @@
 --Data extract for -Labs_PICSURE_API_101.ipynb
 
 --Using this script data will be extracted for PL Picsure Laboratory Results.
---ALS-1723
-/*
-root.bch_hpds_data  --Orignal HPDS data extract.
-root.Bch_Hpds_Data_Lab_App_All -- Data uploaded from API extract.
-root.bch_hpds_data_lab -- Lab nodes grouped as categorical values from the API extract.
-root.bch_hpds_data_get_list --Has data aggregated at concept_path level 
-root.bch_hpds_data_get_list_func  -- for performance improvement of function created distinct concept_path and categorical values.
-root.bch_hpds_data_get_list_map -- Pre populated categorical_values for all the concept_paths for performance.
-root.bch_hpds_data_lab_extract -- Final extract to be generated from this table.
+-- Data extracted has to be saved as data_lab_to_csv.csv
 
+--i2b2_blue.bch_hpds_data  --Orignal HPDS data extract.
+--root.Bch_Hpds_Data_Lab_App_All -- Data uploaded from API extract.
+--root.bch_hpds_data_lab -- Lab nodes grouped as categorical values from the API extract.
+--root.bch_hpds_data_get_list --Has data aggregated at concept_path level 
+--root.bch_hpds_data_get_list_func  -- for performance improvement of function created distinct concept_path and categorical values.
+--root.bch_hpds_data_get_list_map -- Pre populated categorical_values array for all the concept_paths for performance.
+--root.bch_hpds_data_lab_extract -- Final extract to be generated from this table.
+--
 --Database functions
-Get_format_min_max  
-Get_tval_array
-*/
-
---Create table root.Bch_Hpds_Data_Lab_App_All
---And upload API data from Lab Picsure API - Labs_PICSURE_API_101.ipynb ( data_lab_to_csv.csv )
-
-CREATE TABLE ROOT.BCH_HPDS_DATA_LAB_APP_ALL 
-   (	KEY VARCHAR2(4000 BYTE), 
-	PATIENTCOUNT VARCHAR2(4000 BYTE), 
-	CATEGORICAL VARCHAR2(4000 BYTE), 
-	CATEGORYVALUES LONG, 
-	OBSERVATIONCOUNT VARCHAR2(4000 BYTE), 
-	HPDSDATATYPE VARCHAR2(4000 BYTE), 
-	MIN VARCHAR2(4000 BYTE), 
-	MAX VARCHAR2(4000 BYTE)
+--Get_format_min_max  
+--Get_tval_array
+----
+Create Table Root.Bch_Hpds_Data_Lab_App_All 
+   (	Key Varchar2(4000 Byte), 
+	Patientcount Varchar2(4000 Byte), 
+	Categorical Varchar2(4000 Byte), 
+	Categoryvalues Long, 
+	Observationcount Varchar2(4000 Byte), 
+	Hpdsdatatype Varchar2(4000 Byte), 
+	Min Varchar2(4000 Byte), 
+	Max Varchar2(4000 Byte)
    );
-   
+
+--Accounting for the rows for appropriate type.   
+
 create table root.bch_hpds_data_lab as
 (select  PATIENT_NUM ,
 	CONCEPT_PATH ,
 	NVAL_NUM ,
 	TVAL_CHAR ,start_date
-from root.bch_hpds_data a , root.Bch_Hpds_Data_Lab_App_All  b
+from I2B2_BLUE.bch_hpds_data a , root.Bch_Hpds_Data_Lab_App_All  b
 where concept_path like '\Laboratory Results\%'
 and a.concept_path = b.key 
 and B.Categorical = 'False' 
@@ -50,7 +48,7 @@ select  PATIENT_NUM ,
 	CONCEPT_PATH ,
 	NVAL_NUM ,
 	TVAL_CHAR ,start_date
-from  ROOT.bch_hpds_data a, root.Bch_Hpds_Data_Lab_App_All  b
+from  I2B2_BLUE.bch_hpds_data a, root.Bch_Hpds_Data_Lab_App_All  b
 where  concept_path like '\Laboratory Results\%'
 and a.concept_path = b.key 
 and B.Categorical = 'True'
@@ -64,14 +62,16 @@ select  PATIENT_NUM ,
 	CONCEPT_PATH ,
 	NVAL_NUM ,
 	TVAL_CHAR ,start_date
-from  ROOT.bch_hpds_data a, root.Bch_Hpds_Data_Lab_App_All  b
+from  I2B2_BLUE.bch_hpds_data a, root.Bch_Hpds_Data_Lab_App_All  b
 where  concept_path like '\Laboratory Results\%' 
 and a.concept_path = b.key 
 and B.Categorical = 'True'
 and (TVAL_CHAR = 'E' and nval_num is null)
 ) ;
+---
 
 ---Patient level data with aggregated values
+--truncate table root.bch_hpds_data_get_list ;
 
 create table root.bch_hpds_data_get_list( CONCEPT_PATH varchar2(4000) , 
 patientCount  varchar2(4000) ,
@@ -104,8 +104,6 @@ end;
 
 begin
 
-
-
 insert into root.bch_hpds_data_get_list( CONCEPT_PATH ,  patientCount, categorical ,categoryValues , observationCount, HpdsDataType,MIN,MAX)
  ( select 
 	 CONCEPT_PATH , count(distinct patient_num) OVER (PARTITION BY CONCEPT_PATH) AS patientCount, 'False' categorical ,null categoryValues , 
@@ -124,17 +122,23 @@ and a.nval_num is not null
 commit;
 end;
 
+drop table root.bch_hpds_data_get_list_func ;
+
 create table root.bch_hpds_data_get_list_func as
 select distinct l.CONCEPT_PATH  ,l.categoryValues 
 from root.bch_hpds_data_get_list l
 where l.categorical = 'True'  ;
 
-truncate table root.bch_hpds_data_get_list_map drop storage ;
+--truncate table root.bch_hpds_data_get_list_map drop storage ;
+create table root.bch_hpds_data_get_list_map ( concept_path varchar2(4000),
+categoryValues long );
 
+--select * from root.bch_hpds_data_get_list_map
 
 insert into root.bch_hpds_data_get_list_map ( concept_path )
 select distinct concept_path from  root.bch_hpds_data_get_list_func ;
 
+--Function to Pre calculate categoryValues list for all the categorical nodes.
 CREATE OR REPLACE FUNCTION Get_tval_array (P_ConceptPath varchar2)
    return long
 is
@@ -160,14 +164,13 @@ when others then
  return  v_str ;
 end Get_tval_array;
 
-
+--Pre-Populate root.bch_hpds_data_get_list_map with categoryValues.
 DECLARE
   P_CONCEPTPATH VARCHAR2(200);
   v_Return LONG;
 BEGIN
 
-for r_data in ( select concept_path from root.bch_hpds_data_get_list_map
- ) loop
+for r_data in ( select concept_path from root.bch_hpds_data_get_list_map ) loop
 
   P_CONCEPTPATH := r_data.concept_path ;
 
@@ -184,7 +187,7 @@ end loop;
 commit;
 END;
 
-
+--Function to get min and max values for numerical nodes.
 CREATE OR REPLACE FUNCTION Get_format_min_max (p_value varchar2)
    return varchar2
 is
@@ -205,43 +208,48 @@ END IF;
  return  v_str ;
 end Get_format_min_max;
 
+--drop   TABLE ROOT.BCH_HPDS_DATA_LAB_EXTRACT ;
+--truncate table ROOT.BCH_HPDS_DATA_LAB_EXTRACT  ;
 
-  CREATE TABLE ROOT.BCH_HPDS_DATA_LAB_EXTRACT 
-   (	KEY VARCHAR2(4000), 
-	PATIENTCOUNT VARCHAR2(4000), 
-	CATEGORICAL VARCHAR2(4000), 
-	CATEGORYVALUES VARCHAR2(4000), 
-	OBSERVATIONCOUNT VARCHAR2(4000), 
-	HPDSDATATYPE VARCHAR2(4000), 
-	MIN VARCHAR2(4000), 
-	MAX VARCHAR2(4000), 
-	CATEGORYVALUES_1 LONG
-   );
+create table Root.Bch_Hpds_Data_Lab_Extract 
+   (	Key Varchar2(4000), 
+	Patientcount Varchar2(4000), 
+	Categorical Varchar2(4000), 
+	Categoryvalues Varchar2(4000),
+	Observationcount Varchar2(4000), 
+	Hpdsdatatype Varchar2(4000), 
+	Min Varchar2(4000), 
+	Max Varchar2(4000), 
+	Categoryvalues_1 Long );
    
 --truncate table ROOT.BCH_HPDS_DATA_LAB_EXTRACT  ;
 
 begin
 
-insert into root.BCH_HPDS_DATA_LAB_EXTRACT ( KEY ,  patientCount, categorical , categoryValues, observationCount, HpdsDataType ,min ,max )
-SELECT distinct '"'||CONCEPT_PATH||'"' KEY ,  patientCount patientCount, categorical categorical, null categoryValues 
+insert into root.BCH_HPDS_DATA_LAB_EXTRACT ( KEY ,  patientCount, categorical , categoryValues_1, observationCount, HpdsDataType ,min ,max )
+SELECT distinct CONCEPT_PATH KEY ,  patientCount patientCount, categorical categorical, null categoryValues 
  , observationCount observationCount, HpdsDataType HpdsDataType,Get_format_min_max (min) min ,Get_format_min_max (max) max 
 from root.bch_hpds_data_get_list where categorical = 'False';
 
 commit;
 end;
 
+declare
+v_categoryValues long ;
 
 begin
-    for r_data in ( select distinct '"'||l.CONCEPT_PATH||'"' concept_path ,  l.patientCount, l.categorical , l.observationCount, l.HpdsDataType,l.MIN, l.MAX  
-from root.bch_hpds_data_get_list l
-where l.categorical = 'True'  ) loop
-           for r_data_map in (  select  * from root.bch_hpds_data_get_list_map m where r_data.concept_path = '"'||m.CONCEPT_PATH||'"' and rownum = 1  )  loop
-              insert into root.BCH_HPDS_DATA_LAB_EXTRACT ( KEY ,  patientCount, categorical , categoryValues, observationCount, HpdsDataType ,min ,max )
-              values ( r_data.concept_path ,   r_data.patientCount,  r_data.categorical ,  r_data_map.categoryValues,  r_data.observationCount,  r_data.HpdsDataType , r_data.min , r_data.max ); 
-           end loop;
+    for r_data in ( select distinct  concept_path ,  l.patientCount, l.categorical , l.observationCount, l.HpdsDataType,l.MIN, l.MAX  
+                    from root.bch_hpds_data_get_list l
+                    where l.categorical = 'True'  ) loop
+
+              select categoryValues into v_categoryValues from root.bch_hpds_data_get_list_map where concept_path = r_data.concept_path and rownum = 1;
+              
+              insert into root.BCH_HPDS_DATA_LAB_EXTRACT ( KEY ,  patientCount, categorical , categoryValues_1, observationCount, HpdsDataType ,min ,max )
+              values ( r_data.concept_path ,   r_data.patientCount,  r_data.categorical ,  v_categoryValues,  r_data.observationCount,  r_data.HpdsDataType , r_data.min , r_data.max ); 
     
     end loop;
 commit;
 end;
+
 
 
