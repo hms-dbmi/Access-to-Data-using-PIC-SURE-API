792847db71b7c032516f01d15a9a8fa8e20e86fc
Simran Makwana <simranmakw@gmail.com>
2021-04-20 14:49:09 +0000

    Add Rstudio_lib folder

---
+++ NHLBI_BioData_Catalyst/Rstudio/Rstudio_lib/requirements.R
@@ -0,0 +1,33 @@
+list_packages <- c("ggplot2",
+                   "dplyr",
+                   "tidyr",
+                   "urltools",
+                   "devtools",
+                   "ggrepel")
+
+install.packages("cli")
+install.packages("usethis")
+install.packages("openssl")
+
+cat("installing: \n")
+for (package in list_packages)  {
+    cat("- ", package, "\n")
+}
+
+for (package in list_packages){
+     if(! package %in% installed.packages()){
+         install.packages(package, dependencies = TRUE)
+    }
+}
+
+for (package in list_packages) {
+    library(package, character.only = TRUE)
+}
+if (getRversion() < "3.5.0") {
+    isFALSE = function(x) {
+      is.logical(x) && length(x) == 1L && !is.na(x) && !x
+    }
+    isTRUE = function(x) {
+      is.logical(x) && length(x) == 1L && !is.na(x) && x
+    }
+}
